9e2be1e9d52868ed3304ff51e8e0ec6c3fbe0a36
GA <igoutta@users.noreply.github.com>
2024-05-29 15:54:01 -0500

    Update Controller.
    New finished controller. Reading SBUS instead PWM inputs. Better readibility. New reverse system to leave aside ESCs connections.

--- boat_controller.ino
+++ boat_controller.ino
@@ -1,6 +1,8 @@
-#include <Arduino.h>
+#include <sbus.h>
 
-const unsigned int SerialRate = 115200;
+const unsigned int SERIAL_RATE = 115200;
+const unsigned char RX_S2 = 16;
+const unsigned char TX_S2 = 17;
 
 const unsigned int PWM_FREQ = 50;     // 50Hz
 const unsigned char PWM_RESOLUTION = 16; // ESP32 can go up to 16 bits 
@@ -9,111 +11,114 @@ const unsigned int MAX_DUTY_CYCLE = PWM_CYCLE_TICKS - 1;  //Max tick to write
 const unsigned long PWM_CYCLE_USEC = (unsigned long)(1E6/PWM_FREQ); // Total time to complete a cycle in microseconds
 const unsigned int DELAY_MS = (unsigned int)(2E3/PWM_FREQ); //40 ms
 
-struct RCScaler {
-  const uint8_t PIN;
-  const unsigned short MinUS;
-  const unsigned short MaxUS;
-  const unsigned short TrimUS;
-  const unsigned short MinScaledUS;
-  const unsigned short MaxScaledUS;
-  const unsigned short TrimScaledUS;
-  const uint8_t Deadband; //Calculated value of deadzone percentaje to filter inputs vallues and output safe PWM values  
-  volatile unsigned long StartTime;
-  volatile unsigned long CurrentTime;
-  unsigned int ScaledPulse;
+struct rcScaler {
+  const unsigned short minUS;
+  const unsigned short maxUS;
+  const unsigned short trimUS;
+  const unsigned short minScaledUS;
+  const unsigned short maxScaledUS;
+  const unsigned short trimScaledUS;
+  const unsigned char deadzone; //Calculated value of deadzone percentaje to filter inputs values of Trim.
+  unsigned short pulseUS;
+  unsigned short scaledPulseUS;
+  unsigned short reversedPulseUS;
 };
 
-struct Servo {
-  const uint8_t PIN;
-  const uint8_t PWM_CHANNEL;    // ESP32 has 16 channels which can generate 16 independent waveforms
+struct escWriter {
+  const unsigned char pin;
   unsigned int usToTicks;
+  bool reversed;
 };
 
-RCScaler RC_CCW = {32, 1015, 2000, 1500, 1100, 1900, 1500, 25, 0, 0, 0};
-RCScaler RC_CW = {33, 1015, 2000, 1500, 1900, 1100, 1500, 25, 0, 0, 0};
-RCScaler THREE_SETS = {25, 1000, 2000, 1500, 1100, 1900, 1500, 5, 0, 0, 0};
-Servo CCW = {12, 1, 0};
-Servo CW = {14, 0, 0};
+bfs::SbusRx RC_RX(&Serial2, RX_S2, TX_S2, true);
+bfs::SbusData RCData;
+
+rcScaler Throttle = {200,1800,1000, 1100, 1900, 1500, 40, 0, 0, 0};
+rcScaler Steering = {200,1800,1000, 1100, 1900, 1500, 40, 0, 0, 0};
+rcScaler AutoThrottle = {200,1800,1000, 1100, 1900, 1500, 40, 0, 0, 0};
+
+escWriter CCW = {12,  0, false};
+escWriter CW = {14, 0, true};
 
 void setup() {
-  Serial.begin(SerialRate);
+  Serial.begin(SERIAL_RATE);
+  while (!Serial) {}
+  RC_RX.Begin();
 
-  pinMode(RC_CCW.PIN, INPUT_PULLUP);
-  attachInterruptArg(RC_CCW.PIN, readPulse, &RC_CCW, CHANGE);
-  pinMode(RC_CW.PIN, INPUT_PULLUP);
-  attachInterruptArg(RC_CW.PIN, readPulse, &RC_CW, CHANGE);
-  pinMode(THREE_SETS.PIN, INPUT_PULLUP);
-  attachInterruptArg(THREE_SETS.PIN, readPulse, &THREE_SETS, CHANGE);
-
-  ledcSetup(CW.PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcAttachPin(CW.PIN,CW.PWM_CHANNEL);
-  ledcSetup(CCW.PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcAttachPin(CCW.PIN,CCW.PWM_CHANNEL);
+  ledcAttach(CCW.pin, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttach(CW.pin, PWM_FREQ, PWM_RESOLUTION);
 }
 
 void loop() {
-  scalePulse(&RC_CCW);
-  scalePulse(&RC_CW);
-  scalePulse(&THREE_SETS);
-  Serial.print(RC_CCW.CurrentTime);
-  Serial.print(",");
-  Serial.print(RC_CW.CurrentTime);
-  Serial.print(",");
-  Serial.print(THREE_SETS.CurrentTime);
-  Serial.print(",");
-  if(THREE_SETS.ScaledPulse != THREE_SETS.TrimScaledUS) {
-    updateTicks(&CCW, THREE_SETS.ScaledPulse);
-    updateTicksReversed(&CW, THREE_SETS.ScaledPulse);
+  if (RC_RX.Read()) {
+    RCData = RC_RX.data();
+
+    Throttle.pulseUS = RCData.ch[2];
+    Steering.pulseUS = RCData.ch[0];
+    AutoThrottle.pulseUS = RCData.ch[6];
+  }
+  scalePulse(&AutoThrottle);
+  scalePulse(&Throttle);
+  scalePulse(&Steering);
+
+  if(AutoThrottle.scaledPulseUS != AutoThrottle.trimScaledUS) {
+    if (CCW.reversed) { 
+      updateTicks(&CCW, AutoThrottle.scaledPulseUS);
+    } else { //CCW is reversed by default in the steering control
+      updateTicks(&CCW, AutoThrottle.reversedPulseUS);
+    }
+    if (CW.reversed) {
+      updateTicks(&CW, AutoThrottle.reversedPulseUS);
+    } else {  //CW is by default in the positive control
+      updateTicks(&CW, AutoThrottle.scaledPulseUS);
+    }
   }
   else {
-    updateTicks(&CCW, RC_CCW.ScaledPulse);
-    updateTicks(&CW, RC_CW.ScaledPulse);
+    if (CCW.reversed) { 
+      updateTicks(&CCW, (Throttle.scaledPulseUS + Steering.scaledPulseUS)/2);
+    } else {  //CCW is reversed by default in the steering control
+      updateTicks(&CCW, (Throttle.reversedPulseUS + Steering.reversedPulseUS)/2);
+    }
+    if (CW.reversed) {
+      updateTicks(&CW, (Throttle.reversedPulseUS + Steering.reversedPulseUS)/2);
+    } else {  //CW is by default in the positive control
+      updateTicks(&CW, (Throttle.scaledPulseUS + Steering.scaledPulseUS)/2);
+    }
   }
+  writeToMotor(&CCW);
+  writeToMotor(&CW);
+
+  Serial.print(AutoThrottle.pulseUS);
+  Serial.print(",");
+  Serial.print(Throttle.pulseUS);
+  Serial.print(",");
+  Serial.print(Steering.pulseUS);
+  Serial.print(",");
   Serial.print((CCW.usToTicks*float(PWM_CYCLE_USEC))/float(PWM_CYCLE_TICKS));
   Serial.print(",");
   Serial.println((CW.usToTicks*float(PWM_CYCLE_USEC))/float(PWM_CYCLE_TICKS));
-  writeToMotor(&CCW);
-  writeToMotor(&CW);
 }
 
-void ARDUINO_ISR_ATTR readPulse(void* arg) {
-  RCScaler* rc = static_cast<RCScaler*>(arg);
-  if (digitalRead(rc->PIN) == HIGH) {
-    rc->StartTime = micros();
+void scalePulse(rcScaler* rc) {
+  if (rc->pulseUS < rc->minUS || rc->pulseUS > rc->maxUS || abs(rc->trimUS - rc->pulseUS) < rc->deadzone) {
+    rc->scaledPulseUS = rc->trimScaledUS;
+    rc->reversedPulseUS = rc->trimScaledUS;
   } else {
-    rc->CurrentTime = micros() - rc->StartTime;
-  }
-}
-
-void scalePulse(RCScaler* rc) {
-  noInterrupts();
-  if (rc->CurrentTime < (rc->MinUS - rc->Deadband) || rc->CurrentTime > (rc->MaxUS + rc->Deadband)) {
-    rc->ScaledPulse = rc->TrimScaledUS;
-  } else {
-    if (abs(rc->TrimUS- (int)rc->CurrentTime) < rc->Deadband) {
-      rc->ScaledPulse = rc->TrimScaledUS;
+    if (rc->pulseUS < rc->trimUS) {
+      rc->scaledPulseUS = map(rc->pulseUS, rc->minUS, rc->trimUS, rc->minScaledUS, rc->trimScaledUS);
+      rc->reversedPulseUS = map(rc->pulseUS, rc->minUS, rc->trimUS, rc->maxScaledUS, rc->trimScaledUS);
     } else {
-      if (rc->CurrentTime < rc->TrimUS) {
-        rc->ScaledPulse = map(rc->CurrentTime, rc->MinUS, rc->TrimUS, rc->MinScaledUS + rc->Deadband, rc->TrimScaledUS);
-      } else {
-        rc->ScaledPulse = map(rc->CurrentTime, rc->TrimUS, rc->MaxUS, rc->TrimScaledUS, rc->MaxScaledUS - rc->Deadband);
-      }
+      rc->scaledPulseUS = map(rc->pulseUS, rc->trimUS, rc->maxUS, rc->trimScaledUS, rc->maxScaledUS);
+      rc->reversedPulseUS = map(rc->pulseUS, rc->trimUS, rc->maxUS, rc->trimScaledUS, rc->minScaledUS);
     }
   }
-  interrupts();
 }
 
-void updateTicks(Servo* servo, int value) {
-  servo->usToTicks = (unsigned int)((float(value)/float(PWM_CYCLE_USEC))*float(PWM_CYCLE_TICKS));
+void updateTicks(escWriter* motor, int value) {
+  motor->usToTicks = (unsigned int)((float(value)/float(PWM_CYCLE_USEC))*float(PWM_CYCLE_TICKS));
 }
 
-void updateTicksReversed(Servo* servo, int value) {
-  int reversed_value = 1500 - (value - 1500); //Trim-range
-  servo->usToTicks = (unsigned int)((float(reversed_value)/float(PWM_CYCLE_USEC))*float(PWM_CYCLE_TICKS));
-}
-
-
-void writeToMotor(Servo* servo) {
-  ledcWrite(servo->PWM_CHANNEL, servo->usToTicks);
+void writeToMotor(escWriter* motor) {
+  ledcWrite(motor->pin, motor->usToTicks);
   delay(DELAY_MS);
 }
